a017322f1a51b831749f0943b3a23094afd136df
Simão Pedro M. Gabriel <spedromg@hotmail.com>
2025-01-01 09:06:12 -0300

    update banner ao abrir a página.

--- principal.js
+++ principal.js
@@ -1,8 +1,9 @@
-var banners = [“Os melhores do Brasil!”, “Qualidade e preço baixo!”];
+var banners = ['Os melhores do Brasil!', 'Qualidade e preço baixo!'];
 var bannerAtual = 0;
+
 function trocarBanner() {
 	bannerAtual = (bannerAtual + 1) % 2;
-document.querySelector(‘h2#mensagem’).textContent = banners[bannerAtual];
+	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
 }
 
 setInterval(trocarBanner, 2000);
